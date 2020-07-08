6271ebd3fd4cfd2c14c439487f07ba5f48ca434e
Starblastr <62027236+Starblastr@users.noreply.github.com>
2020-07-08 18:00:23 -0400

    Add files via upload

--- store/urls.py
+++ store/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
 from allauth.account.views import LoginView, LogoutView
 from . import views
-
+from . import product_views
 
 urlpatterns = [
     path('', views.store, name='home'),
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('accounts/logout/', LogoutView.as_view(), name='logout'),
     path('accounts/signup/success', views.home, name='success'),
-    path('store/', views.store, name="store"),
-    path('cart/', views.cart, name="cart"),
     path('blog/', views.blog, name='blog'),
+    path('store/', views.store, name="store"),
+    path('supplements/', product_views.supplements, name='supplements' ),
+
+    path('cart/', views.cart, name="cart"),
+    path('commment-pending/', views.comment_pending, name='comment_pending'),
     path('checkout/', views.checkout, name="checkout"),
     path('update_item/', views.updateItem, name='update_item'),
     path('process_order/', views.processOrder, name='process_order'),
@@ -19,8 +22,10 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('policy/', views.policy, name='policy'),
     path('contact/', views.contact, name='contact'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path('<slug:slug>/', views.ItemDetail.as_view(), name='item_detail'),
+path('<slug:slug>/c', product_views.SupplementDetail.as_view(), name='supplement_detail' ),
+    path('<slug:slug>/a', views.post_detail, name='post_detail'),
+    path('<slug:slug>/b', views.ItemDetail.as_view(), name='item_detail'),
+
 
 
 ]
